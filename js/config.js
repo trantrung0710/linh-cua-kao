173f2f4f3ad58a0f1e799d6f7131ddc52fa64b90
Tx3 Developers <84718052+trunglaptrinh@users.noreply.github.com>
2021-10-25 08:41:52 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'helu cậu!',
+    introDesc: `mình có điều này muốn thổ lộ với bạn`,
+    btnIntro: 'oke',
+    title: 'Linh làm người yêu Trung nhé <3',
+    desc: 'Linh không trả lời hoặc thoát ra, tức là Linh đồng ý đó nha :> ',
+    btnYes: 'Linh đồng ý  <3',
+    btnNo: 'Bạn mơ à :3',
+    question: 'và...Linh sẽ xưng hô thế nào ',
+    btnReply: 'hmm ??',
+    reply: 'Trung nè, Linh yêu cậu ?',
+    mess: 'Anh biết mà . Yêu Em ',
+    messDesc: 'call cho anh nhá.',
+    btnAccept: 'Oki',
+    messLink: 'https://m.me/tx3developers'
+}
